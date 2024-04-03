fef86521588f0a5259377a9b87b9e018bbcaf4f8
sulaymonz <sulaymonz@gmail.com>
2024-04-04 02:20:08 +0500

    Optimized the carousel image lazy loading

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -6,20 +6,27 @@ const Carousel = ({ slides, current }) => {
       className="absolute top-0 flex h-full w-full transition ease-in-out duration-[250ms]"
       style={{ transform: `translateX(-${100 * current}%)` }}
     >
-      {slides.map((slide, i) => (
-        <div
-          key={slide.src}
-          className="h-full aspect-[5/6] bg-center bg-contain bg-no-repeat"
-          suppressHydrationWarning
-          style={
-            Math.abs(current - i) <= 2
-              ? {
-                  backgroundImage: `url("/images/design_samples/${slide.src}")`,
-                }
-              : {}
-          }
-        />
-      ))}
+      {slides.map((slide, i) => {
+        const inLoadingZone = Math.abs(current - i) <= 2;
+        const node = (
+          <div
+            key={slide.src}
+            className="h-full aspect-[5/6] bg-center bg-contain bg-no-repeat"
+            suppressHydrationWarning
+            style={
+              inLoadingZone || slide.imageLoaded
+                ? {
+                    backgroundImage: `url("/images/design_samples/${slide.src}")`,
+                  }
+                : {}
+            }
+          />
+        );
+        if (inLoadingZone) {
+          slide.imageLoaded = true;
+        }
+        return node;
+      })}
     </div>
   );
 };
